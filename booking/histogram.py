e5dfc09b403b325cae41601f75415cbaf82bfba6
Xingzhong <xu.xeno@gmail.com>
2014-05-14 18:33:31 -0700

    update the comment

--- booking/histogram.py
+++ booking/histogram.py
@@ -1,14 +1,24 @@
+# post processing script
+# file format convert from csv to excel through pandas 
+# create histogram through pandas 
+# -- Bo Yang
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
+	# load data 
 	df = pd.read_csv('../data.csv', header=0, 
 			names=['Review Score','Review Count','Name'])
+	# convert to excel
 	df.to_excel('../data.xlsx', sheet_name='Paris')
+	# create figure
 	fig = plt.figure()
 	ax = fig.add_subplot()
+	# create histogram
 	df['Review Score'].hist(alpha=0.7, ax=ax,
 			bins=20, normed=False, rwidth=0.8, align='mid')
 	fig.suptitle("Paris Hotels Review Score Distributions")
+	# save the figure
 	fig.savefig("../histogram.png")
 	
--- booking/spiders/booking_spider.py
+++ booking/spiders/booking_spider.py
@@ -1,3 +1,6 @@
+# Spider to parse the booking.com
+# use the offset parameter trick in its url format
+
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from booking.items import BookingItem
@@ -6,12 +9,20 @@ from booking.items import BookingItem
 class BookingSpider(BaseSpider):
     name = "Booking"
     allowed_domains = ["booking.com"]
+
+    # create a url lists for the first 1000+ hotels
+    # offset is the parameter of the hotel order
     start_urls = map(lambda o:
     	"http://www.booking.com/searchresults.html?city=-1456928;offset=%s;rows=20"%o,
     	range(0,1900,20))
 
     def parse(self, response):
+        # for each downloaded page, call function parse
+
+        # structure the raw data to html structure
         sel = HtmlXPathSelector(response)
+
+        # get lists of hotels sub-div 
         hotels = sel.select("//div[@class='sr_item_content']")
         items = []
         for hotel in hotels:
